c6ed8dd9a4cb54b7f2077abc7e9e4a8e8fc306ea
Kurra Sushanth <kurrasushanth@gmail.com>
2025-08-15 14:45:06 -0700

    error correction

--- frontend/src/app/services/topic.service.ts
+++ frontend/src/app/services/topic.service.ts
@@ -1,7 +1,7 @@
 // src/app/services/topic.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment'; 
+import { environment } from '../../environments/environment.prod'; 
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +10,19 @@ export class TopicService {
   constructor(private http: HttpClient) {}
 
   getTopics() {
-    return this.http.get(`${environment.apiUrl}/topics`);
+    return this.http.get(`${environment.apiBaseUrl}/topics`);
   }
 
   getQuestions(topicId: number) {
-    return this.http.get(`${environment.apiUrl}/topics/${topicId}/questions`);
+    return this.http.get(`${environment.apiBaseUrl}/topics/${topicId}/questions`);
   }
 
   createTopic(data: any) {
-    return this.http.post(`${environment.apiUrl}/topics`, data);
+    return this.http.post(`${environment.apiBaseUrl}/topics`, data);
   }
 
   addQuestion(topicId: number, question: string) {
-    return this.http.post(`${environment.apiUrl}/topics/${topicId}/questions`, {
+    return this.http.post(`${environment.apiBaseUrl}/topics/${topicId}/questions`, {
       text: question,
     });
   }
