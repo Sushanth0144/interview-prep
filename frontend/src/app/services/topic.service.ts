9e51e31abce905aa65bf8f16625458d6ec886cad
Kurra Sushanth <kurrasushanth@gmail.com>
2025-08-19 16:42:15 -0700

    Use environments/environment (no .prod) so fileReplacements work

--- frontend/src/app/services/topic.service.ts
+++ frontend/src/app/services/topic.service.ts
@@ -1,7 +1,7 @@
 // src/app/services/topic.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.prod'; 
+import { environment } from '../../environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
